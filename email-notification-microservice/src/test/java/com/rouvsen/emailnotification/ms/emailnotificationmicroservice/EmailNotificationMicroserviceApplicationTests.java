8a09ad89c39b6520e6bc8c1ab0f04639515f05ff
rouvsen <rouvsen@gmail.com>
2024-11-26 01:43:05 +0400

    initial commit for email-notification-microservice

---
+++ email-notification-microservice/src/test/java/com/rouvsen/emailnotification/ms/emailnotificationmicroservice/EmailNotificationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rouvsen.emailnotification.ms.emailnotificationmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailNotificationMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
